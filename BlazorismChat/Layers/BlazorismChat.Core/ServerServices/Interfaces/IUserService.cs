29a67b76492ad74602ed1bf0916dd5e98fa934aa
Taha Ghadirian <taha.ghadirian@outlook.com>
2021-09-10 16:37:44 +0430

    Add User Services.

---
+++ BlazorismChat/Layers/BlazorismChat.Core/ServerServices/Interfaces/IUserService.cs
@@ -0,0 +1,29 @@
+﻿using BlazorismChat.DbLayer.Entities.Users;
+using System.Threading.Tasks;
+
+namespace BlazorismChat.Core.ServerServices.Interfaces;
+
+public interface IUserService
+{
+    Task<bool> IsUsernameExists(string username);
+    Task<bool> IsEmailExists(string email);
+    Task<bool> IsUserExists(int userId);
+
+    Task<User?> GetUserByUsername(string username);
+    Task<User?> GetUserByEmail(string email);
+    Task<User?> GetUserById(int userId);
+
+    Task<User> CreateUser(User user);
+    Task<User?> UpdateUser(User user, bool updateSecurityKey = false);
+    Task<bool> DeleteUser(User user, bool fullyDelete = false);
+    Task<bool> DeleteUser(int userId, bool fullyDelete = false);
+
+    Task<bool> UpdateUserPassword(User user, string password);
+    Task<bool> UpdateUserPassword(string username, string password);
+
+    Task<User?> RegisterUser(User user);
+    Task<User?> LoginUser(string username, string password);
+
+    Task<List<User>> GetAllUsers();
+}
+
---
+++ BlazorismChat/Layers/BlazorismChat.Core/ServerServices/UserService.cs
@@ -0,0 +1,173 @@
+using BlazorismChat.Core.ServerServices.Interfaces;
+using BlazorismChat.DbLayer.Entities.Users;
+using BlazorismChat.DbLayer.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using BlazorismChat.ClientLibraries.Convertors;
+using BlazorismChat.Core.Security;
+
+namespace BlazorismChat.Core.ServerServices;
+
+public class UserService : IUserService
+{
+    private readonly BlazorismChatDbContext _dbContext;
+
+    public UserService(BlazorismChatDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<User> CreateUser(User user)
+    {
+        var result = await _dbContext.Users.AddAsync(user);
+        await _dbContext.SaveChangesAsync();
+        return result.Entity;
+    }
+
+    public async Task<bool> DeleteUser(User user, bool fullyDelete = false)
+    {
+        try
+        {
+            if (user == null)
+                return false;
+
+            if (fullyDelete)
+            {
+                _dbContext.Users.Remove(user);
+                await _dbContext.SaveChangesAsync();
+            }
+            else
+            {
+                user.IsDeleted = true;
+                var resultUser = await UpdateUser(user, true);
+                return resultUser != null;
+            }
+
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> DeleteUser(int userId, bool fullyDelete = false)
+    {
+        var user = await GetUserById(userId);
+        if (user == null)
+            return false;
+
+        return await DeleteUser(user, fullyDelete);
+    }
+
+    public async Task<List<User>> GetAllUsers()
+    {
+        return await _dbContext.Users.ToListAsync();
+    }
+
+    public async Task<User?> GetUserByEmail(string email)
+    {
+        return await _dbContext.Users.FirstOrDefaultAsync(x => x.FixedEmail == TextFixer.FixEmail(email));
+    }
+
+    public async Task<User?> GetUserById(int userId)
+    {
+        var user = await _dbContext.Users.FindAsync(userId);
+        
+        if(user == null)
+            return null;
+
+        return user.IsDeleted ? null : user;
+    }
+
+    public async Task<User?> GetUserByUsername(string username)
+    {
+        return await _dbContext.Users.FirstOrDefaultAsync(x => x.FixedUserName == TextFixer.FixUserName(username));
+    }
+
+    public async Task<bool> IsEmailExists(string email)
+    {
+        return await _dbContext.Users.AnyAsync(x => x.FixedEmail == TextFixer.FixEmail(email));
+    }
+
+    public async Task<bool> IsUserExists(int userId)
+    {
+        return await _dbContext.Users.AnyAsync(x => x.UserId == userId);
+    }
+
+    public async Task<bool> IsUsernameExists(string username)
+    {
+        return await _dbContext.Users.AnyAsync(x => x.FixedUserName == TextFixer.FixUserName(username));
+    }
+
+    public async Task<User?> LoginUser(string username, string password)
+    {
+        if (!await IsUsernameExists(username))
+            return null;
+
+        var encPass = PasswordHelper.EncodePasswordMd5(password);
+        return await _dbContext.Users.FirstOrDefaultAsync(x => x.FixedUserName == TextFixer.FixUserName(username) && x.Password == encPass);
+    }
+
+    public async Task<User?> RegisterUser(User user)
+    {
+        if (await IsUsernameExists(user.UserName) || await IsEmailExists(user.Email))
+            return null;
+
+        user.Password = PasswordHelper.EncodePasswordMd5(user.Password);
+
+#if DEBUG
+        user.IsEmailConfirmed = true;
+        user.IsPhoneNumberConfirmed = true;
+#endif
+
+        return await CreateUser(user);
+    }
+
+    public async Task<User?> UpdateUser(User user, bool updateSecurityKey = false)
+    {
+        try
+        {
+            if (updateSecurityKey)
+                user.ActiveCode = NameGenerator.GenerateUniqueCode();
+
+            user.IdentityCode = NameGenerator.GenerateUniqueCode();
+
+            _dbContext.Entry(user).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+            return user;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public async Task<bool> UpdateUserPassword(User user, string password)
+    {
+        try
+        {
+            if (user == null || user == new User())
+                return false;
+
+            if (string.IsNullOrWhiteSpace(password))
+                return false;
+
+            user.Password = PasswordHelper.EncodePasswordMd5(password);
+            var result = await UpdateUser(user, true);
+            return result != null;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> UpdateUserPassword(string username, string password)
+    {
+        var user = await GetUserByUsername(username);
+        if (user == null)
+            return false;
+
+        return await UpdateUserPassword(user, password);
+    }
+}
