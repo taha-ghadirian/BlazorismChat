6b68557994365e1c2371d09e6753f713fb483e99
Taha Ghadirian <taha.ghadirian@outlook.com>
2021-09-27 17:54:54 +0330

    implement DataAnotations.

--- BlazorismChat/Layers/BlazorismChat.ClientLibraries/DTOs/AccountDTOs.cs
+++ BlazorismChat/Layers/BlazorismChat.ClientLibraries/DTOs/AccountDTOs.cs
@@ -1,16 +1,46 @@
+using System.ComponentModel.DataAnnotations;
 namespace BlazorismChat.ClientLibraries.DTOs;
 
 public class LoginDTO
 {
+    [Display(Name = "Username", Prompt = "Username...")]
+    [Required(ErrorMessage = "{0} is required")]
+    [MinLength(3, ErrorMessage = "{0} must be at least {1} characters long")]
+    [MaxLength(60, ErrorMessage = "{0} must be at most {1} characters long")]
     public string UserName { get; set; }
+
+    [Display(Name = "Password", Prompt = "Password...")]
+    [Required(ErrorMessage = "{0} is required")]
+    [MinLength(8, ErrorMessage = "{0} must be at least {1} characters long")]
+    [MaxLength(60, ErrorMessage = "{0} must be at most {1} characters long")]
     public string Password { get; set; }
+
     public bool RememberMe { get; set; }
 }
 
 public class RegisterDTO
 {
+    [Display(Name = "Username", Prompt = "Username...")]
+    [Required(ErrorMessage = "{0} is required")]
+    [MinLength(3, ErrorMessage = "{0} must be at least {1} characters long")]
+    [MaxLength(60, ErrorMessage = "{0} must be at most {1} characters long")]
     public string UserName { get; set; }
+
+    [Display(Name = "Email", Prompt = "example@gmail.com")]
+    [Required(ErrorMessage = "{0} is required")]
+    [EmailAddress(ErrorMessage = "Invalid {0}")]
     public string Email { get; set; }
+
+    [Display(Name = "Password", Prompt = "Password...")]
+    [Required(ErrorMessage = "{0} is required")]
+    [MinLength(8, ErrorMessage = "{0} must be at least {1} characters long")]
+    [MaxLength(60, ErrorMessage = "{0} must be at most {1} characters long")]
     public string Password { get; set; }
+
+    [Display(Name = "Confirm Password", Prompt = "Repeat password")]
+    [Required(ErrorMessage = "{0} is required")]
+    [MinLength(8, ErrorMessage = "{0} must be at least {1} characters long")]
+    [MaxLength(60, ErrorMessage = "{0} must be at most {1} characters long")]
+    [Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
     public string ConfirmPassword { get; set; }
 }
--- BlazorismChat/Layers/BlazorismChat.ClientLibraries/DTOs/AuthenticationDTOs.cs
+++ BlazorismChat/Layers/BlazorismChat.ClientLibraries/DTOs/AuthenticationDTOs.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorismChat.ClientLibraries.DTOs;
 
 public class AuthenticationResponse
@@ -18,7 +20,15 @@ public class AuthenticationRequest
         
     }
 
+    [Display(Name = "Username", Prompt = "Username...")]
+    [Required(ErrorMessage = "{0} is required")]
+    [MinLength(3, ErrorMessage = "{0} must be at least {1} characters long")]
+    [MaxLength(60, ErrorMessage = "{0} must be at most {1} characters long")]
     public string UserName { get; set; }
 
+    [Display(Name = "Password", Prompt = "Password...")]
+    [Required(ErrorMessage = "{0} is required")]
+    [MinLength(8, ErrorMessage = "{0} must be at least {1} characters long")]
+    [MaxLength(60, ErrorMessage = "{0} must be at most {1} characters long")]
     public string Password { get; set; }
 }
